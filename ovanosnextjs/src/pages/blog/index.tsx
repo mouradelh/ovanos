c3675ab5b4b54519573957841162e3edb2f499b7
Mourad El Harchaoui <elharchaoui.mourad@gmail.com>
2023-12-31 03:54:09 +0100

    gewerkt aan blogs

--- ovanosnextjs/src/pages/blog/index.tsx
+++ ovanosnextjs/src/pages/blog/index.tsx
@@ -9,14 +9,14 @@ import { AllBlogPosts } from "../api/hello"
 import { BlogPosts } from ".."
 
 interface BlogCardProps {
+    id: number,
     title: string,
     datum: string,
     imageUrl: string,
-    link: string,
     korteBeschrijving: string
 }
 
-export function BlogCard({title, datum, imageUrl, korteBeschrijving, link}:BlogCardProps) {
+export function BlogCard({title, datum, imageUrl, korteBeschrijving, id}:BlogCardProps) {
     return(
     <div className="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
       <div className="flex flex-col space-y-1.5 p-6">
@@ -33,7 +33,7 @@ export function BlogCard({title, datum, imageUrl, korteBeschrijving, link}:BlogC
       </div>
       <div className="flex items-center p-6">
         <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 px-4 py-2">
-          <Link legacyBehavior href="">
+          <Link legacyBehavior href={`/blog/${id}`}>
           <a className="text-blue-500 hover:underline">
             Read More
           </a>
@@ -59,6 +59,7 @@ export function BlogPost() {
             {
                 blogPosts?.data.map((post) => (
                     <BlogCard
+                        id={post.id}
                         title={post.attributes.Title} 
                         datum={post.attributes.Datum} 
                         link="#" 
@@ -78,6 +79,7 @@ export default function Blog() {
         <Header></Header>
         <Navigation></Navigation>
         <main>
+          <h1 className="text-center text-4xl font-bold">Blog Posts</h1>
             {BlogPost()}
         </main>
         <Footer></Footer>
--- ovanosnextjs/src/pages/index.tsx
+++ ovanosnextjs/src/pages/index.tsx
@@ -76,10 +76,10 @@ export function BlogpostCard({id, title, description, imageUrl}: BlogPostCardPro
 
 export function RecentBlogPostCard({title, description, imageUrl, id}: BlogPostCardProps) {
   return(
-    <Link legacyBehavior href={"/"}>
-    <a key={id} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+    <Link legacyBehavior href={`/blog/${id}`}>
+    <a key={id} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 text-center">
       <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={imageUrl} alt={title}/>
-      <div className="flex flex-col justify-between p-4 leading-normal">
+      <div className="p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{title}</h5>
         <p className="mb-3 font-normal text-gray-700 ">{description}</p>
       </div>
@@ -126,9 +126,7 @@ export default function Home() {
             <button className="bg-blue-500 text-white px-4 py-2 rounded"><a href="/menu">Ons Menu</a></button>
           </div>
           <h2 className='text-center font-extrabold'>Recentste Blogpost</h2>
-          <div className='flex flex-row'>
           <BlogPosts></BlogPosts>
-          </div>
           
           <Info></Info>
         </main>
